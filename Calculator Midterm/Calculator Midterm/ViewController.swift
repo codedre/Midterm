688210fb83ed0a4d17129f9b319f934bbadb1651
Rob Wyant <rob@getyapper.com>
2015-02-18 17:29:26 -0500

    adding 'PerformOperation' class and some code to 'ViewController' to pull PerformOperation class into the current view.
    :q
    q
    git status

--- Calculator Midterm/Calculator Midterm/ViewController.swift
+++ Calculator Midterm/Calculator Midterm/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userIsInTheMiddleOfTypingNumber = false
+    var display: UILabel = UILabel(frame:CGRect(x: 0, y: 0, width: 0, height: 0))
+
+    var brain = PerformOperations()
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        } set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false
+        }
+    }
+
+    
+    func appendDigit(sender:UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+    }
+    
+    func operate (sender:UIButton) {
+        if userIsInTheMiddleOfTypingNumber {
+            enter(sender)
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    func enter(sender:UIButton) {
+        userIsInTheMiddleOfTypingNumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+        
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
